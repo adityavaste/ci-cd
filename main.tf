a7edc1a897721e426210ad23a7e7a8b1ff6fe67c
adityavaste <adityavaste@gmail.com>
2025-12-15 12:19:14 +0530

    jenkinsfile

---
+++ main.tf
@@ -0,0 +1,15 @@
+module "eks-cluster" {
+  source  = "https://github.com/adityavaste/eks_module.git?ref=dev"
+  version = "v1"
+
+
+  deployHub_vpc_cidr_block         = "10.0.0.0/16"
+  public_subnet_cide_block         = "10.0.1.0/24"
+  public_subnet_availability_zone  = "ap-south-1a"
+  private_subnet_cide_block        = "10.0.2.0/24"
+  private_subnet_availability_zone = "ap-south-1b"
+  aws_instance_ami                 = "ami-00ca570c1b6d79f36"
+  aws_instance_type                = "t2.micro"
+  aws_instance_key_pair            = "12-dec"
+
+}
